92890edcbc99cfabcf1f2e367f89581c122219d4
Debmalya Datta <111640763+debmalyadatta@users.noreply.github.com>
2022-08-20 22:55:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ except URLError as e:
   streamlit.error()
 
 # Snowflake connectivity
-streamlit.stop()
+#streamlit.stop()
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
